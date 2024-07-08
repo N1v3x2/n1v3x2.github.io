87d5eb2b61a69f6d6fceb43c38a1cc77279b8714
Kevin Zhang <kevzhang2022@gmail.com>
2024-07-07 20:10:13 -0600

    Remove unused variabale from Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import banner_pic from "../images/banner_pic.jpeg";
 import "./Home.css";
 
 const Home = () => (
